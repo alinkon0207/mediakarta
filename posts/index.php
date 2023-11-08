15580cff3ebb1563e5adf6fe6511dc540a1c6018
ilesoviy <ilisovij861@gmail.com>
2023-11-08 22:12:09 +0900

    Simplified source

--- posts/index.php
+++ posts/index.php
@@ -1,9 +1,10 @@
+
 <?php
     include('../bootstrap.php');
 ?>
 
 <!DOCTYPE html>
-<html>
+<html lang="en">
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -31,198 +32,17 @@
         <script src="<?php echo BASE_URL; ?>/public/bootstrap/js/bootstrap.bundle.min.js"></script>
         <script src="<?php echo BASE_URL; ?>/public/plugins/src/table/datatable/datatables.js"></script>
     </head>
-
+    
     <body class="layout-boxed" monica-version="3.1.2" monica-id="ofpnmcalabcbjgholdjcjblkibolbppb">
-        <div class="header-container container-xxl">
-            <header class="header navbar navbar-expand-sm">
-                <a href="javascript:void(0);" class="sidebarCollapse">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-menu">
-                        <line x1="3" y1="12" x2="21" y2="12"></line>
-                        <line x1="3" y1="6" x2="21" y2="6"></line>
-                        <line x1="3" y1="18" x2="21" y2="18"></line>
-                    </svg>
-                </a>
-                <div class="search-animated toggle-search">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-search">
-                        <circle cx="11" cy="11" r="8"></circle>
-                        <line x1="21" y1="21" x2="16.65" y2="16.65"></line>
-                    </svg>
-                    <form class="form-inline search-full form-inline search" role="search">
-                        <div class="search-bar">
-                            <input type="text" class="form-control search-form-control  ml-lg-auto" placeholder="Search...">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x search-close">
-                                <line x1="18" y1="6" x2="6" y2="18"></line>
-                                <line x1="6" y1="6" x2="18" y2="18"></line>
-                            </svg>
-                        </div>
-                    </form>
-                    <span class="badge badge-secondary">Ctrl + /</span>
-                </div>
-                <ul class="navbar-item flex-row ms-lg-auto ms-0">
-                    <li class="nav-item dropdown user-profile-dropdown order-lg-0 order-1">
-                        <a href="javascript:void(0);" class="nav-link dropdown-toggle user" id="userProfileDropdown" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            <div class="avatar-container">
-                                <div class="avatar avatar-sm avatar-indicators avatar-online">
-                                    <img alt="avatar" src="<?php echo BASE_URL; ?>/public/assets/img/avatar.png" class="rounded-circle">
-                                </div>
-                            </div>
-                        </a>
-                        <div class="dropdown-menu position-absolute" aria-labelledby="userProfileDropdown">
-                            <div class="user-profile-section">
-                                <div class="media mx-auto">
-                                    <div class="emoji me-2">👋</div>
-                                    <div class="media-body">
-                                        <h5> <?php $GLOBALS['USERNAME']; ?> </h5>
-                                        <p> <?php $GLOBALS['ROLE']; ?> </p>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="dropdown-item">
-                                <a href="<?php echo BASE_URL; ?>/settings">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-settings">
-                                        <circle cx="12" cy="12" r="3"></circle>
-                                        <path d="M19.4 15a1.65 1.65 0 0 0 .33 1.82l.06.06a2 2 0 0 1 0 2.83 2 2 0 0 1-2.83 0l-.06-.06a1.65 1.65 0 0 0-1.82-.33 1.65 1.65 0 0 0-1 1.51V21a2 2 0 0 1-2 2 2 2 0 0 1-2-2v-.09A1.65 1.65 0 0 0 9 19.4a1.65 1.65 0 0 0-1.82.33l-.06.06a2 2 0 0 1-2.83 0 2 2 0 0 1 0-2.83l.06-.06a1.65 1.65 0 0 0 .33-1.82 1.65 1.65 0 0 0-1.51-1H3a2 2 0 0 1-2-2 2 2 0 0 1 2-2h.09A1.65 1.65 0 0 0 4.6 9a1.65 1.65 0 0 0-.33-1.82l-.06-.06a2 2 0 0 1 0-2.83 2 2 0 0 1 2.83 0l.06.06a1.65 1.65 0 0 0 1.82.33H9a1.65 1.65 0 0 0 1-1.51V3a2 2 0 0 1 2-2 2 2 0 0 1 2 2v.09a1.65 1.65 0 0 0 1 1.51 1.65 1.65 0 0 0 1.82-.33l.06-.06a2 2 0 0 1 2.83 0 2 2 0 0 1 0 2.83l-.06.06a1.65 1.65 0 0 0-.33 1.82V9a1.65 1.65 0 0 0 1.51 1H21a2 2 0 0 1 2 2 2 2 0 0 1-2 2h-.09a1.65 1.65 0 0 0-1.51 1z"></path>
-                                    </svg>
-                                    <span>Settings</span>
-                                </a>
-                            </div>
-                            <div class="dropdown-item">
-                                <a href="<?php echo BASE_URL; ?>/logout" class="text-danger">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-log-out">
-                                        <path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4"></path>
-                                        <polyline points="16 17 21 12 16 7"></polyline>
-                                        <line x1="21" y1="12" x2="9" y2="12"></line>
-                                    </svg>
-                                    <span>Log Out</span>
-                                </a>
-                            </div>
-                        </div>
-                    </li>
-                </ul>
-            </header>
-        </div>
+
+        <?php include '../topbar.html'; ?>
+        
         <div class="main-container" id="container">
             <div class="overlay"></div>
             <div class="search-overlay"></div>
 
-            <div class="sidebar-wrapper sidebar-theme">
-                <nav id="sidebar">
-                    <div class="navbar-nav theme-brand flex-row  text-center">
-                        <div class="nav-logo">
-                            <div class="nav-item theme-logo">
-                                <a href="<?php echo BASE_URL; ?>/dashboard">
-                                    <img src="<?php echo BASE_URL; ?>/public/assets/img/nisy64x64.png" class="navbar-logo" alt="logo">
-                                </a>
-                            </div>
-                            <div class="nav-item theme-text">
-                                <a href="<?php echo BASE_URL; ?>/dashboard" class="nav-link"> Nisy Portal </a>
-                            </div>
-                        </div>
-                        <div class="nav-item sidebar-toggle">
-                            <div class="btn-toggle sidebarCollapse">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevrons-left">
-                                    <polyline points="11 17 6 12 11 7"></polyline>
-                                    <polyline points="18 17 13 12 18 7"></polyline>
-                                </svg>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="profile-info">
-                        <div class="user-info">
-                            <div class="profile-img">
-                                <img src="<?php echo BASE_URL; ?>/public/assets/img/avatar.png" alt="avatar">
-                            </div>
-                            <div class="profile-content">
-                                <h6 class=""> <?php echo $GLOBALS['USERNAME']; ?> </h6>
-                                <p class=""> <?php echo $GLOBALS['ROLE']; ?> </p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="shadow-bottom"></div>
-                    <ul class="list-unstyled menu-categories ps" id="accordionNisy">
-                        <li class="menu ">
-                            <a href="<?php echo BASE_URL; ?>/dashboard" aria-expanded="false" class="dropdown-toggle">
-                                <div class="">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-home">
-                                        <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path>
-                                        <polyline points="9 22 9 12 15 12 15 22"></polyline>
-                                    </svg>
-                                    <span>Dashboard</span>
-                                </div>
-                            </a>
-                        </li>
-                        <li class="menu ">
-                            <a href="#posts" data-bs-toggle="collapse" aria-expanded="false" class="dropdown-toggle">
-                                <div class="">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-pen-tool">
-                                        <path d="M12 19l7-7 3 3-7 7-3-3z"></path>
-                                        <path d="M18 13l-1.5-7.5L2 2l3.5 14.5L13 18l5-5z"></path>
-                                        <path d="M2 2l7.586 7.586"></path>
-                                        <circle cx="11" cy="11" r="2"></circle>
-                                    </svg>
-                                    <span>Posts</span>
-                                </div>
-                                <div>
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-right">
-                                        <polyline points="9 18 15 12 9 6"></polyline>
-                                    </svg>
-                                </div>
-                            </a>
-                            <ul class="submenu list-unstyled collapse" id="posts" data-bs-parent="#accordionNisy" style="">
-                                <li class="">
-                                    <a href="<?php echo BASE_URL; ?>/posts"> Posts </a>
-                                </li>
-                                <li class="">
-                                    <a href="<?php echo BASE_URL; ?>/posts/create"> Create Post </a>
-                                </li>
-                            </ul>
-                        </li>
-                            <li class="menu ">
-                            <a href="<?php echo BASE_URL; ?>/logs" aria-expanded="false" class="dropdown-toggle">
-                                <div class="">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-file-text">
-                                        <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path>
-                                        <polyline points="14 2 14 8 20 8"></polyline>
-                                        <line x1="16" y1="13" x2="8" y2="13"></line>
-                                        <line x1="16" y1="17" x2="8" y2="17"></line>
-                                        <polyline points="10 9 9 9 8 9"></polyline>
-                                    </svg>
-                                    <span>Target Logs</span>
-                                </div>
-                            </a>
-                        </li>
-                        <li class="menu ">
-                            <a href="<?php echo BASE_URL; ?>/settings" aria-expanded="false" class="dropdown-toggle">
-                                <div class="">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-settings">
-                                        <circle cx="12" cy="12" r="3"></circle>
-                                        <path d="M19.4 15a1.65 1.65 0 0 0 .33 1.82l.06.06a2 2 0 0 1 0 2.83 2 2 0 0 1-2.83 0l-.06-.06a1.65 1.65 0 0 0-1.82-.33 1.65 1.65 0 0 0-1 1.51V21a2 2 0 0 1-2 2 2 2 0 0 1-2-2v-.09A1.65 1.65 0 0 0 9 19.4a1.65 1.65 0 0 0-1.82.33l-.06.06a2 2 0 0 1-2.83 0 2 2 0 0 1 0-2.83l.06-.06a1.65 1.65 0 0 0 .33-1.82 1.65 1.65 0 0 0-1.51-1H3a2 2 0 0 1-2-2 2 2 0 0 1 2-2h.09A1.65 1.65 0 0 0 4.6 9a1.65 1.65 0 0 0-.33-1.82l-.06-.06a2 2 0 0 1 0-2.83 2 2 0 0 1 2.83 0l.06.06a1.65 1.65 0 0 0 1.82.33H9a1.65 1.65 0 0 0 1-1.51V3a2 2 0 0 1 2-2 2 2 0 0 1 2 2v.09a1.65 1.65 0 0 0 1 1.51 1.65 1.65 0 0 0 1.82-.33l.06-.06a2 2 0 0 1 2.83 0 2 2 0 0 1 0 2.83l-.06.06a1.65 1.65 0 0 0-.33 1.82V9a1.65 1.65 0 0 0 1.51 1H21a2 2 0 0 1 2 2 2 2 0 0 1-2 2h-.09a1.65 1.65 0 0 0-1.51 1z"></path>
-                                    </svg>
-                                    <span>Settings</span>
-                                </div>
-                            </a>
-                        </li>
-                        <li class="menu ">
-                            <a href="<?php echo BASE_URL; ?>/knowledgebase" aria-expanded="false" class="dropdown-toggle">
-                                <div class="">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-help-circle">
-                                        <circle cx="12" cy="12" r="10"></circle>
-                                        <path d="M9.09 9a3 3 0 0 1 5.83 1c0 2-3 3-3 3"></path>
-                                        <line x1="12" y1="17" x2="12.01" y2="17"></line>
-                                    </svg>
-                                    <span>Knowledge Base</span>
-                                </div>
-                            </a>
-                        </li>
-                        <div class="ps__rail-x" style="left: 0px; bottom: 0px;">
-                            <div class="ps__thumb-x" tabindex="0" style="left: 0px; width: 0px;"></div>
-                        </div>
-                        <div class="ps__rail-y" style="top: 0px; right: 0px;">
-                            <div class="ps__thumb-y" tabindex="0" style="top: 0px; height: 0px;"></div>
-                        </div>
-                    </ul>
-                </nav>
-            </div>
+            <?php include '../sidebar.html'; ?>
+
             <div id="content" class="main-content">
                 <div class="layout-px-spacing">
                     <div class="middle-content container-xxl p-0">
