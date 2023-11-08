af409ae26f5aee4a4cc0f527c2175ea472a7b0a6
ilesoviy <ilisovij861@gmail.com>
2023-11-09 03:18:13 +0900

    Simplified with footer.html

--- posts/create/index.php
+++ posts/create/index.php
@@ -66,7 +66,7 @@
                                         </div>
                                         <div class="row mb-4">
                                             <div class="col-sm-12">
-                                                <textarea id="summernote" name="content" style="display: none;">This is summernote example</textarea>
+                                                <textarea id="summernote" name="content" style="display: none;">This is summernote creation example</textarea>
                                             </div>
                                         </div>
                                     </div>
@@ -115,19 +115,8 @@
                         </form>
                     </div>
                 </div>
-                <div class="footer-wrapper">
-                    <div class="footer-section f-section-1">
-                        <p class="">Copyright © <span class="dynamic-year">2023</span> <a target="_blank" href="#">Nisy</a>. All Rights Reserved.</p>
-                    </div>
-                    <div class="footer-section f-section-2">
-                        <p class="">Made with 
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-heart">
-                                <path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path>
-                            </svg>
-                            in Bandung
-                        </p>
-                    </div>
-                </div>
+                
+                <?php include('../footer.html'); ?>
             </div>
         </div>
         <script src="<?php echo BASE_URL; ?>/public/plugins/src/perfect-scrollbar/perfect-scrollbar.min.js"></script>
--- posts/index.php
+++ posts/index.php
@@ -110,7 +110,7 @@
                                                         <td>Terbaru</td>
                                                         <td class="sorting_1">2023-06-12 08:24:32</td>
                                                         <td>
-                                                            <a class="badge badge-light-primary text-start me-2 action-edit" href="<?php echo BASE_URL; ?>/posts/edit/c6d8857dd7e0ac40">
+                                                            <a class="badge badge-light-primary text-start me-2 action-edit" href="<?php echo BASE_URL; ?>/posts/edit/index.php?id=c6d8857dd7e0ac40">
                                                                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-edit-3">
                                                                     <path d="M12 20h9"></path>
                                                                     <path d="M16.5 3.5a2.121 2.121 0 0 1 3 3L7 19l-4 1 1-4L16.5 3.5z"></path>
@@ -192,15 +192,8 @@
                         </script> -->
                     </div>
                 </div>
-                <div class="footer-wrapper">
-                <div class="footer-section f-section-1">
-                <p class="">Copyright © <span class="dynamic-year">2023</span> <a target="_blank" href="#">Nisy</a>. All Rights Reserved.</p>
-                </div>
-                <div class="footer-section f-section-2">
-                <p class="">Made with <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-heart"><path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path></svg> in Bandung</p>
-                </div>
-                </div>
-                </div>
+                
+                <?php include '../footer.html'; ?>
             </div>
             <script src="<?php echo BASE_URL; ?>/public/plugins/src/perfect-scrollbar/perfect-scrollbar.min.js"></script>
             <script src="<?php echo BASE_URL; ?>/public/plugins/src/mousetrap/mousetrap.min.js"></script>
@@ -216,7 +209,8 @@
                     position: {
                         x: 'right',
                         y: 'top',
-                    }});
+                    }
+                });
             </script>
             <div class="notyf"></div>
             <div class="notyf-announcer" aria-atomic="true" aria-live="polite" style="border: 0px; clip: rect(0px, 0px, 0px, 0px); height: 1px; margin: -1px; overflow: hidden; padding: 0px; position: absolute; width: 1px; outline: 0px;"></div>
