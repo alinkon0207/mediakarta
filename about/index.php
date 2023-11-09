58dc1fd9ecbcfa73639be0b665f71a44807ae7fb
ilesoviy <ilisovij861@gmail.com>
2023-11-09 09:46:07 +0900

    Designed about/index.php

---
+++ about/index.php
@@ -0,0 +1,59 @@
+<?php
+	include('../bootstrap.php');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<meta name="description" content="Informasi lengkapnya mengenai Mediakarta - Kabar Terkini, Berita Terupdate oleh Mediakarta.">
+		<meta name="keywords" content="Bisnis,Bola,Edukasi,Ekonomi,Entrepreneur,Hiburan,Hukum,Internasional,Investment,Kecantikan,Kesehatan,Lifestyle,Market,Metro,Nasional,Olahraga,Opini,Otomotif,Politik,Profil,Sains,Selebriti,Syariah,Teknologi,Terbaru,Travel">
+		<meta name="author" content="mediakarta.com">
+		<title>Mediakarta - Kabar Terkini, Berita Terupdate</title>
+		<link rel="manifest" href="<?php echo BASE_URL; ?>/mediakarta.webmanifest">
+		<link rel="shortcut icon" href="<?php echo BASE_URL; ?>/favicon.ico" type="image/x-icon">
+		<link rel="icon" href="<?php echo BASE_URL; ?>/favicon.ico" type="image/x-icon">
+		<!--<link rel="apple-touch-icon" sizes="180x180" href="<?php echo BASE_URL; ?>/apple-touch-icon.png">-->
+		<link rel="stylesheet" href="<?php echo BASE_URL; ?>/public/news/plugins/bootstrap/css/bootstrap.min.css">
+		<link rel="stylesheet" href="<?php echo BASE_URL; ?>/public/news/plugins/font-awesome/css/font-awesome.min.css">
+		<link rel="stylesheet" href="<?php echo BASE_URL; ?>/public/news/plugins/slick-carousel/slick.css">
+		<link rel="stylesheet" href="<?php echo BASE_URL; ?>/public/news/plugins/slick-carousel/slick-theme.css?v=1.0.0">
+		<link rel="stylesheet" href="<?php echo BASE_URL; ?>/public/news/css/style.css">
+		<script src="<?php echo BASE_URL; ?>/public/news/plugins/jquery/jquery.js"></script>
+		<script src="<?php echo BASE_URL; ?>/public/news/plugins/bootstrap/js/bootstrap.min.js"></script>
+		<script>var bodyWeb = document.getElementsByTagName('body');</script>
+	</head>
+	
+	<body style="" monica-version="3.1.2" monica-id="ofpnmcalabcbjgholdjcjblkibolbppb">
+		<?php include('../header.html'); ?>
+		
+		<section class="section-padding">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                        <div class="all-news-block">
+                            <h2 class="text-center">
+                                <b>About Us</b>
+                            </h2>
+                            <hr>
+                            <div class="all-news">
+                                <p>Website berita adalah platform daring yang menyediakan informasi terkini dan berita terbaru dalam berbagai topik seperti politik, ekonomi, hiburan, olahraga, dan banyak lagi. Melalui website berita, pengguna dapat mengakses artikel-artikel berita terbaru, laporan mendalam, editorial, opini, dan bahkan video yang mengabarkan peristiwa terkini di seluruh dunia. Website berita juga sering menyediakan fitur komentar yang memungkinkan pengguna untuk berinteraksi dan berbagi pendapat mereka terkait berita yang disajikan.</p>
+                                <p>Salah satu contoh website berita terkemuka adalah MediaKarta. MediaKarta adalah sebuah platform berita yang menawarkan liputan terpercaya dan komprehensif dalam berbagai bidang. Dengan tim jurnalis yang berpengalaman, MediaKarta menyajikan berita terbaru, analisis mendalam, dan wawancara eksklusif dengan tokoh-tokoh terkemuka di berbagai sektor. Website MediaKarta juga memberikan ruang bagi para pembaca untuk mengungkapkan pendapat mereka melalui fitur komentar yang interaktif, sehingga tercipta diskusi yang dinamis di antara komunitas pembaca.</p>
+                                <p>Selain itu, MediaKarta juga menawarkan beragam rubrik dan kategori berita, mulai dari politik, ekonomi, teknologi, hiburan, kesehatan, hingga gaya hidup. Dengan tampilan yang user-friendly dan navigasi yang mudah, pengguna dapat dengan cepat menemukan topik berita yang menarik minat mereka. MediaKarta berkomitmen untuk memberikan informasi terkini dan terpercaya kepada pembacanya, serta menjaga standar keberimbangan dan objektivitas dalam menyajikan berita.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+	
+		<?php include('../footer.html'); ?>
+		
+		<script src="<?php echo BASE_URL; ?>/public/news/plugins/slick-carousel/slick.min.js"></script>
+		<script src="<?php echo BASE_URL; ?>/public/news/js/custom.js"></script>
+
+		<div id="monica-content-root" class="monica-widget"></div>
+	</body>
+</html>
