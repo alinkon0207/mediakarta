ce7e6d26ba51aa7103ac25d9295668e6c4934d38
ilesoviy <ilisovij861@gmail.com>
2023-11-09 09:48:16 +0900

    Designed privacy/index.php

---
+++ privacy/index.php
@@ -0,0 +1,82 @@
+<?php
+	include('../bootstrap.php');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<meta name="description" content="Informasi lengkapnya mengenai Mediakarta - Kabar Terkini, Berita Terupdate oleh Mediakarta.">
+		<meta name="keywords" content="Bisnis,Bola,Edukasi,Ekonomi,Entrepreneur,Hiburan,Hukum,Internasional,Investment,Kecantikan,Kesehatan,Lifestyle,Market,Metro,Nasional,Olahraga,Opini,Otomotif,Politik,Profil,Sains,Selebriti,Syariah,Teknologi,Terbaru,Travel">
+		<meta name="author" content="mediakarta.com">
+		<title>Mediakarta - Kabar Terkini, Berita Terupdate</title>
+		<link rel="manifest" href="<?php echo BASE_URL; ?>/mediakarta.webmanifest">
+		<link rel="shortcut icon" href="<?php echo BASE_URL; ?>/favicon.ico" type="image/x-icon">
+		<link rel="icon" href="<?php echo BASE_URL; ?>/favicon.ico" type="image/x-icon">
+		<!--<link rel="apple-touch-icon" sizes="180x180" href="<?php echo BASE_URL; ?>/apple-touch-icon.png">-->
+		<link rel="stylesheet" href="<?php echo BASE_URL; ?>/public/news/plugins/bootstrap/css/bootstrap.min.css">
+		<link rel="stylesheet" href="<?php echo BASE_URL; ?>/public/news/plugins/font-awesome/css/font-awesome.min.css">
+		<link rel="stylesheet" href="<?php echo BASE_URL; ?>/public/news/plugins/slick-carousel/slick.css">
+		<link rel="stylesheet" href="<?php echo BASE_URL; ?>/public/news/plugins/slick-carousel/slick-theme.css?v=1.0.0">
+		<link rel="stylesheet" href="<?php echo BASE_URL; ?>/public/news/css/style.css">
+		<script src="<?php echo BASE_URL; ?>/public/news/plugins/jquery/jquery.js"></script>
+		<script src="<?php echo BASE_URL; ?>/public/news/plugins/bootstrap/js/bootstrap.min.js"></script>
+		<script>var bodyWeb = document.getElementsByTagName('body');</script>
+	</head>
+	
+	<body style="" monica-version="3.1.2" monica-id="ofpnmcalabcbjgholdjcjblkibolbppb">
+		<?php include('../header.html'); ?>
+		
+		<section class="section-padding">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                        <div class="all-news-block">
+                            <h2 class="text-center">
+                                <b>Privacy Policy</b>
+                            </h2>
+                            <hr>
+                            <div class="all-news">
+                                <h3>Siapa kami</h3>
+                                <p>Alamat situs web kami adalah: https://www.mediakarta.com.</p>
+                                
+                                <h3>Komentar</h3>
+                                <p>Saat pengunjung meninggalkan komentar pada situs, kita mengumpulkan data yang ditampilkan pada form komentar, alamat IP pengunjung dan user agent browser untuk membantu pendeteksian spam.</p>
+                                <p>String anonim yang dibuat dari alamat email Anda (juga disebut hash) dapat diberikan ke layanan Gravatar untuk melihat apakah Anda menggunakannya. Kebijakan privasi layanan Gravatar tersedia di sini: https://automattic.com/privacy/. Setelah persetujuan atas komentar Anda, gambar profil Anda dapat dilihat oleh publik dalam konteks komentar Anda.</p>
+                                
+                                <h3>Media</h3>
+                                <p>Jika Anda mengunggah gambar ke situs web, Anda harus menghindari mengunggah gambar dengan data lokasi tertanam (GPS EXIF) yang disertakan. Pengunjung ke situs web dapat mengunduh dan mengekstrak data lokasi apa pun dari gambar di situs web.</p>
+                                
+                                <h3>Cookies</h3>
+                                <p>Jika Anda meninggalkan komentar di situs kami, Anda dapat memilih untuk menyimpan nama, alamat email, dan situs web Anda dalam cookie. Ini untuk kenyamanan Anda sehingga Anda tidak perlu mengisi detail Anda lagi ketika Anda meninggalkan komentar lain. Cookie ini akan bertahan selama satu tahun.</p>
+                                <p>Jika Anda mengunjungi laman login kami, kami akan memasang cookie sementara untuk memastikan apakah browser Anda menerima cookie. Cookie ini tidak mengandung data pribadi dan dibuang ketika Anda menutup browser Anda.</p>
+                                <p>Saat Anda log masuk, kami akan menyiapkan beberapa cookie untuk menyimpan informasi log masuk Anda dan tampilan yang Anda pilih. Cookie log masuk berlaku selama dua hari, dan cookie pengaturan tampilan berlaku selama satu tahun. Jika Anda memilih “Ingatkan Saya”, log masuk anda akan bertahan selama dua minggu. Jika Anda log keluar dari akun, cookie log masuk akan dihapus.</p>
+                                <p>Jika Anda menyunting atau menerbitkan artikel, cookie tambahan akan disimpan di browser Anda. Cookie ini tidak menyertakan data pribadi dan hanya menunjukkan ID posting dari artikel yang baru saja Anda sunting. Kadaluwarsa setelah 1 hari.</p>
+                                
+                                <h3>Konten yang disematkan dari situs web lain</h3>
+                                <p>Artikel-artikel di dalam situs ini dapat menyertakan konten terembed (seperti video, gambar, artikel, dll). Konten terembed dari situs web lain akan berlaku sama dengan pengunjung yang mengunjungi situs web lain.</p>
+                                <p>Situs-situs web ini dapat mengumpulkan data tentang Anda, menggunakan cookies, menanamkan pelacak dari pihak ketiga, dan memonitor interaksi Anda dengan muatan tertanam, termasuk menggunakannya untuk melacak interaksi Anda jika Anda memiliki sebuah akun dan masuk ke dalam situs web tersebut.</p>
+                                
+                                <h3>Berapa lama kami menyimpan data Anda</h3>
+                                <p>Jika Anda meninggalkan komentar, metadatanya disimpan tanpa batas. Ini agar kami dapat mengenali dan menyetujui komentar tindak lanjut secara otomatis dan tidak menahannya dalam antrean moderasi.</p>
+                                <p>Untuk pengguna yang mendaftar pada website kami (jika ada), kami juga menyimpan informasi pribadi yang mereka berikan dalam profil pengguna mereka. Semua pengguna dapat melihat dan mengedit informasi pribadi mereka kapan saja (kecuali mengubah nama pengguna). Administrator situs juga dapat melihat dan mengedit informasi tersebut.</p>
+                                
+                                <h3>Hak apa yang Anda miliki atas data Anda</h3>
+                                <p>Jika anda mempunyai akun di situs ini, atau telah meninggalkan komentar, anda dapat meminta untuk mendapat data personal dalam file export dari kami, termasuk data yang anda berikan kepada kami. Anda juga dapat meminta kami menghapus data personal mengenai anda. Ini tidak termasuk data yang wajib kami simpan untuk keperluan administratif, hukum, atau keamanan.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+	
+		<?php include('../footer.html'); ?>
+		
+		<script src="<?php echo BASE_URL; ?>/public/news/plugins/slick-carousel/slick.min.js"></script>
+		<script src="<?php echo BASE_URL; ?>/public/news/js/custom.js"></script>
+
+		<div id="monica-content-root" class="monica-widget"></div>
+	</body>
+</html>
