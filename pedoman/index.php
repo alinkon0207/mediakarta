b8db014f7ec66c4cf27387222ba78a627558c004
ilesoviy <ilisovij861@gmail.com>
2023-11-09 09:52:02 +0900

    Designed pedoman/index.php

---
+++ pedoman/index.php
@@ -0,0 +1,62 @@
+<?php
+	include('../bootstrap.php');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<meta name="description" content="Informasi lengkapnya mengenai Mediakarta - Kabar Terkini, Berita Terupdate oleh Mediakarta.">
+		<meta name="keywords" content="Bisnis,Bola,Edukasi,Ekonomi,Entrepreneur,Hiburan,Hukum,Internasional,Investment,Kecantikan,Kesehatan,Lifestyle,Market,Metro,Nasional,Olahraga,Opini,Otomotif,Politik,Profil,Sains,Selebriti,Syariah,Teknologi,Terbaru,Travel">
+		<meta name="author" content="mediakarta.com">
+		<title>Mediakarta - Kabar Terkini, Berita Terupdate</title>
+		<link rel="manifest" href="<?php echo BASE_URL; ?>/mediakarta.webmanifest">
+		<link rel="shortcut icon" href="<?php echo BASE_URL; ?>/favicon.ico" type="image/x-icon">
+		<link rel="icon" href="<?php echo BASE_URL; ?>/favicon.ico" type="image/x-icon">
+		<!--<link rel="apple-touch-icon" sizes="180x180" href="<?php echo BASE_URL; ?>/apple-touch-icon.png">-->
+		<link rel="stylesheet" href="<?php echo BASE_URL; ?>/public/news/plugins/bootstrap/css/bootstrap.min.css">
+		<link rel="stylesheet" href="<?php echo BASE_URL; ?>/public/news/plugins/font-awesome/css/font-awesome.min.css">
+		<link rel="stylesheet" href="<?php echo BASE_URL; ?>/public/news/plugins/slick-carousel/slick.css">
+		<link rel="stylesheet" href="<?php echo BASE_URL; ?>/public/news/plugins/slick-carousel/slick-theme.css?v=1.0.0">
+		<link rel="stylesheet" href="<?php echo BASE_URL; ?>/public/news/css/style.css">
+		<script src="<?php echo BASE_URL; ?>/public/news/plugins/jquery/jquery.js"></script>
+		<script src="<?php echo BASE_URL; ?>/public/news/plugins/bootstrap/js/bootstrap.min.js"></script>
+		<script>var bodyWeb = document.getElementsByTagName('body');</script>
+	</head>
+	
+	<body style="" monica-version="3.1.2" monica-id="ofpnmcalabcbjgholdjcjblkibolbppb">
+		<?php include('../header.html'); ?>
+		
+		<section class="section-padding">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                        <div class="all-news-block">
+                            <h2 class="text-center">
+                                <b>Pedoman Media Siber</b>
+                            </h2>
+                            <hr>
+                            <div class="all-news">
+                                <p>Kemerdekaan berpendapat, kemerdekaan berekspresi, dan kemerdekaan pers adalah hak asasi manusia yang dilindungi Pancasila, Undang-Undang Dasar 1945, dan Deklarasi Universal Hak Asasi Manusia PBB.</p>
+                                <p>Keberadaan media siber di Indonesia juga merupakan bagian dari kemerdekaan berpendapat, kemerdekaan berekspresi, dan kemerdekaan pers.</p>
+                                <p>Media siber memiliki karakter khusus sehingga memerlukan pedoman agar pengelolaannya dapat dilaksanakan secara profesional, memenuhi fungsi, hak, dan kewajibannya sesuai Undang-Undang Nomor 40 Tahun 1999 tentang Pers dan Kode Etik Jurnalistik.</p>
+                                <p>Kemerdekaan pers sendiri adalah salah satu wujud kedaulatan rakyat yang berlandaskan prinsip-prinsip demokrasi, keadilan, dan supremasi hukum.</p>
+                                <p>Kemerdekaan pers merupakan salah satu wujud kedaulatan rakyat dan menjadi unsur yang sangat penting untuk menciptakan kehidupan bermasyarakat, berbangsa dan bernegara yang demokratis, sehingga kemerdekaan mengeluarkan pikiran dan pendapat sebagaimana tercantum dalam Pasal 28 Undang-Undang Dasar 1945 harus dijamin.</p>
+                                <p>Dalam kehidupan bermasyarakat, berbangsa, dan bernegara yang demokratis, kemerdekaan menyatakan pikiran dan pendapat sesuai dengan hati nurani dan hak memperoleh informasi, merupakan hak asasi manusia yang sangat hakiki, yang diperlukan untuk menegakkan keadilan dan kebenaran, memajukan kesejahteraan umum, dan mencerdaskan kehidupan bangsa.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+	
+		<?php include('../footer.html'); ?>
+		
+		<script src="<?php echo BASE_URL; ?>/public/news/plugins/slick-carousel/slick.min.js"></script>
+		<script src="<?php echo BASE_URL; ?>/public/news/js/custom.js"></script>
+
+		<div id="monica-content-root" class="monica-widget"></div>
+	</body>
+</html>
