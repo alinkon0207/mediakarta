88a6f3506c2bff7767858ba711eecc6084f3d930
ilesoviy <ilisovij861@gmail.com>
2023-11-09 09:49:44 +0900

    Designed disclaimer/index.php

---
+++ disclaimer/index.php
@@ -0,0 +1,60 @@
+<?php
+	include('../bootstrap.php');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<meta name="description" content="Informasi lengkapnya mengenai Mediakarta - Kabar Terkini, Berita Terupdate oleh Mediakarta.">
+		<meta name="keywords" content="Bisnis,Bola,Edukasi,Ekonomi,Entrepreneur,Hiburan,Hukum,Internasional,Investment,Kecantikan,Kesehatan,Lifestyle,Market,Metro,Nasional,Olahraga,Opini,Otomotif,Politik,Profil,Sains,Selebriti,Syariah,Teknologi,Terbaru,Travel">
+		<meta name="author" content="mediakarta.com">
+		<title>Mediakarta - Kabar Terkini, Berita Terupdate</title>
+		<link rel="manifest" href="<?php echo BASE_URL; ?>/mediakarta.webmanifest">
+		<link rel="shortcut icon" href="<?php echo BASE_URL; ?>/favicon.ico" type="image/x-icon">
+		<link rel="icon" href="<?php echo BASE_URL; ?>/favicon.ico" type="image/x-icon">
+		<!--<link rel="apple-touch-icon" sizes="180x180" href="<?php echo BASE_URL; ?>/apple-touch-icon.png">-->
+		<link rel="stylesheet" href="<?php echo BASE_URL; ?>/public/news/plugins/bootstrap/css/bootstrap.min.css">
+		<link rel="stylesheet" href="<?php echo BASE_URL; ?>/public/news/plugins/font-awesome/css/font-awesome.min.css">
+		<link rel="stylesheet" href="<?php echo BASE_URL; ?>/public/news/plugins/slick-carousel/slick.css">
+		<link rel="stylesheet" href="<?php echo BASE_URL; ?>/public/news/plugins/slick-carousel/slick-theme.css?v=1.0.0">
+		<link rel="stylesheet" href="<?php echo BASE_URL; ?>/public/news/css/style.css">
+		<script src="<?php echo BASE_URL; ?>/public/news/plugins/jquery/jquery.js"></script>
+		<script src="<?php echo BASE_URL; ?>/public/news/plugins/bootstrap/js/bootstrap.min.js"></script>
+		<script>var bodyWeb = document.getElementsByTagName('body');</script>
+	</head>
+	
+	<body style="" monica-version="3.1.2" monica-id="ofpnmcalabcbjgholdjcjblkibolbppb">
+		<?php include('../header.html'); ?>
+		
+		<section class="section-padding">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                        <div class="all-news-block">
+                            <h2 class="text-center">
+                                <b>Disclaimer</b>
+                            </h2>
+                            <hr>
+                            <div class="all-news">
+                                <p>Informasi yang terdapat dalam website ini adalah untuk tujuan umum saja. Informasi ini disediakan oleh Mediakarta.com dan kami senantiasa berusaha untuk menjaga informasi yang aktual dan benar.</p>
+                                <p>Kami tidak membuat pernyataan atau jaminan apapun, tersurat maupun tersirat, tentang akurasi kelengkapan, kesesuaian, atau ketersediaan ke situs web atau informasi, produk, jasa, atau gambar terkait yang terdapat pada website ini untuk tujuan apapun.</p>
+                                <p>Setiap ketergantungan yang anda tempatkan pada informasi tersebut adalah risiko anda sendiri.</p>
+                                <p>Dalam hal apapun kami tidak bertanggung jawab atas kerugian atau kerusakan termasuk tanpa batasan, kerugian tidak langsung atau kerusakan apapun yang timbul dari hilangnya data atau keuntungan yang timbul dari penggunaan website ini.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+	
+		<?php include('../footer.html'); ?>
+		
+		<script src="<?php echo BASE_URL; ?>/public/news/plugins/slick-carousel/slick.min.js"></script>
+		<script src="<?php echo BASE_URL; ?>/public/news/js/custom.js"></script>
+
+		<div id="monica-content-root" class="monica-widget"></div>
+	</body>
+</html>
