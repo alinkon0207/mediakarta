1ca09fab69a8665f29c7cf4c3b0498bbc49cfb9a
ilesoviy <ilisovij861@gmail.com>
2023-11-14 15:25:03 +0900

    Relayout dashboard

--- admin/dashboard/index.php
+++ admin/dashboard/index.php
@@ -23,14 +23,14 @@
         // Prepare select statements
         if ($is_admin) {
             // admin
-            $sql1 = "SELECT COUNT(id) FROM posts";
-            $sql2 = "SELECT COUNT(id) FROM logs";
-            $sql3 = "SELECT COUNT(id) FROM users WHERE role = 'user'";
+            $sql1 = "SELECT COUNT(id) FROM users WHERE role = 'user'";
+            $sql2 = "SELECT COUNT(id) FROM posts";
+            $sql3 = "SELECT COUNT(id) FROM logs";
         } else {
             // user
-            $sql1 = "SELECT COUNT(id) FROM posts WHERE author = '$user_id'";
-            $sql2 = "SELECT COUNT(id) FROM posts, logs WHERE posts.author = '$user_id' AND posts.id = logs.post_id";
-            $sql3 = "SELECT COUNT(id) FROM users WHERE id = '$user_id'";
+            $sql1 = "SELECT COUNT(id) FROM users WHERE id = '$user_id'";
+            $sql2 = "SELECT COUNT(id) FROM posts WHERE author = '$user_id'";
+            $sql3 = "SELECT COUNT(id) FROM posts, logs WHERE posts.author = '$user_id' AND posts.id = logs.post_id";
         }
 
         $stmt1 = mysqli_prepare($conn, $sql1);
@@ -41,7 +41,7 @@
             // Attempt to execute the prepared statement
             if (mysqli_stmt_execute($stmt1)) {
                 mysqli_stmt_store_result($stmt1);
-                mysqli_stmt_bind_result($stmt1, $NUM_POSTS);
+                mysqli_stmt_bind_result($stmt1, $NUM_USERS);
                 mysqli_stmt_fetch($stmt1);
             } else {
                 echo "Oops! Something went wrong. Please try again later. (2-1)";
@@ -49,7 +49,7 @@
 
             if (mysqli_stmt_execute($stmt2)) {
                 mysqli_stmt_store_result($stmt2);
-                mysqli_stmt_bind_result($stmt2, $NUM_LOGS);
+                mysqli_stmt_bind_result($stmt2, $NUM_POSTS);
                 mysqli_stmt_fetch($stmt2);
             } else {
                 echo "Oops! Something went wrong. Please try again later. (2-2)";
@@ -57,7 +57,7 @@
 
             if (mysqli_stmt_execute($stmt3)) {
                 mysqli_stmt_store_result($stmt3);
-                mysqli_stmt_bind_result($stmt3, $NUM_USERS);
+                mysqli_stmt_bind_result($stmt3, $NUM_LOGS);
                 mysqli_stmt_fetch($stmt3);
             } else {
                 echo "Oops! Something went wrong. Please try again later. (2-3)";
@@ -135,6 +135,26 @@
                                     <div class="widget-heading">
                                         <div class="w-title m-0">
                                             <div class="w-icon">
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-users">
+                                                    <path d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path>
+                                                    <circle cx="9" cy="7" r="4"></circle>
+                                                    <path d="M23 21v-2a4 4 0 0 0-3-3.87"></path>
+                                                    <path d="M16 3.13a4 4 0 0 1 0 7.75"></path>
+                                                </svg>
+                                            </div>
+                                            <div class="">
+                                                <p class="w-value"> <?php echo $NUM_USERS; ?> </p>
+                                                <h5 class="">Users</h5>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-xl-4 col-lg-12 col-md-12 col-sm-12 col-12 layout-spacing">
+                                <div class="widget-content widget-one_hybrid widget-content-area rounded">
+                                    <div class="widget-heading">
+                                        <div class="w-title m-0">
+                                            <div class="w-icon">
                                                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-pen-tool">
                                                     <path d="M12 19l7-7 3 3-7 7-3-3z"></path>
                                                     <path d="M18 13l-1.5-7.5L2 2l3.5 14.5L13 18l5-5z"></path>
@@ -166,26 +186,6 @@
                                             <div class="">
                                                 <p class="w-value"> <?php echo $NUM_LOGS ?> </p>
                                                 <h5 class="">Target Logs</h5>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-xl-4 col-lg-12 col-md-12 col-sm-12 col-12 layout-spacing">
-                                <div class="widget-content widget-one_hybrid widget-content-area rounded">
-                                    <div class="widget-heading">
-                                        <div class="w-title m-0">
-                                            <div class="w-icon">
-                                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-users">
-                                                    <path d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path>
-                                                    <circle cx="9" cy="7" r="4"></circle>
-                                                    <path d="M23 21v-2a4 4 0 0 0-3-3.87"></path>
-                                                    <path d="M16 3.13a4 4 0 0 1 0 7.75"></path>
-                                                </svg>
-                                            </div>
-                                            <div class="">
-                                                <p class="w-value"> <?php echo $NUM_USERS; ?> </p>
-                                                <h5 class="">Users</h5>
                                             </div>
                                         </div>
                                     </div>
