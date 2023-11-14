4c56cd79c37329ea49749f906020a9b3d0eb6173
ilesoviy <ilisovij861@gmail.com>
2023-11-15 04:53:46 +0900

    Implemented user pages

--- bootstrap.php
+++ bootstrap.php
@@ -1,2 +1,17 @@
 <?php
     define('BASE_URL', '/mediakarta');
+
+    define('DB_HOST', 'localhost');
+    define('DB_USER', 'root');
+    define('DB_PASS', '');
+    define('DB_NAME', 'bms_db');
+
+    define('DAYS_PER_MONTH', 31);
+	define('MONTHS_PER_YEAR', 12);
+	define('DAYS_PER_YEAR', 365);
+	
+	function getDaysDiff($difference) {
+		if ($difference < DAYS_PER_MONTH) return $difference . ' day(s)';
+		else if ($difference < DAYS_PER_MONTH * MONTHS_PER_YEAR) return floor($difference / DAYS_PER_MONTH) . ' month(s)';
+		else return floor($difference / DAYS_PER_MONTH / MONTHS_PER_YEAR) . ' year(s)';
+	}
--- index.php
+++ index.php
@@ -1,5 +1,76 @@
 <?php
 	include('bootstrap.php');
+
+	define('NEWS_PER_PAGE', 3 * 3);
+
+	if (isset($_GET['page'])) {
+		$page = $_GET['page'];
+	} else {
+		$page = 1;
+	}
+
+	/* Get latest news */
+	global $conn;
+
+	$conn = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+	if (!$conn) {
+		die("Cannot Establish A Secure Connection To The Host Server At The Moment!");
+	}
+
+	// count pages
+	$sql = "SELECT id, category, title, permalink, DATEDIFF(NOW(), date) AS date_differ, contents FROM posts " . 
+		"WHERE date >= DATE_SUB(NOW(), INTERVAL 6 MONTH)";
+
+	if ($stmt = mysqli_prepare($conn, $sql)) {
+		mysqli_stmt_execute($stmt);
+		mysqli_stmt_store_result($stmt);
+		$records = mysqli_stmt_num_rows($stmt);
+
+		$npages = intdiv($records + 8, 9);
+
+		mysqli_stmt_close($stmt);
+	} else {
+		$npages = 0;
+		$pages = 0;
+	}
+
+	// fetch records
+	$sql = "SELECT id, category, title, permalink, DATEDIFF(NOW(), date) AS date_differ, contents FROM posts " . 
+		"WHERE date >= DATE_SUB(NOW(), INTERVAL 6 MONTH) " . 
+		"LIMIT " . ($page - 1) * NEWS_PER_PAGE . ', ' . NEWS_PER_PAGE;
+
+	$news = "";
+
+	if ($stmt = mysqli_prepare($conn, $sql)) {
+		mysqli_stmt_execute($stmt);
+		mysqli_stmt_store_result($stmt);
+		$records = mysqli_stmt_num_rows($stmt);
+		
+		$result = mysqli_query($conn, $sql);
+
+		while ($row = mysqli_fetch_assoc($result)) {
+			$news = $news . 
+			'<div class="col-lg-4 col-md-6">
+				<div class="post-block-wrapper post-float-half clearfix">
+					<div class="post-content">
+						<a class="post-category" href="#">' . $row['category']. '</a>
+						<h2 class="post-title mt-3">
+							<a href="' . BASE_URL . '/read.php?title=' . $row['permalink'] . '">' . $row['title']. '</a>
+						</h2>
+						<div class="post-meta">
+							<span class="posted-time">' . getDaysDiff($row['date_differ']) . '</span>
+						</div>
+						<p>' . $row['contents'] . '</p>
+					</div>
+				</div>
+			</div>';
+		}
+
+		mysqli_stmt_close($stmt);
+	}
+
+	// Close connection
+	mysqli_close($conn);
 ?>
 
 <!DOCTYPE html>
@@ -13,7 +84,7 @@
 		<meta name="author" content="mediakarta.com">
 		<title>Mediakarta - Kabar Terkini, Berita Terupdate</title>
 		<link rel="manifest" href="<?php echo BASE_URL; ?>/mediakarta.webmanifest">
-		<!-- <link rel="shortcut icon" href="<?php echo BASE_URL; ?>/logo.png" type="image/png"> -->
+		<link rel="shortcut icon" href="<?php echo BASE_URL; ?>/logo.png" type="image/png">
 		<link rel="icon" href="<?php echo BASE_URL; ?>/logo.png" type="image/png">
 		<!--<link rel="apple-touch-icon" sizes="180x180" href="<?php echo BASE_URL; ?>/apple-touch-icon.png">-->
 		<link rel="stylesheet" href="<?php echo BASE_URL; ?>/public/news/plugins/bootstrap/css/bootstrap.min.css">
@@ -39,151 +110,35 @@
 							</h3>
 							<div class="all-news">
 								<div class="row">
-									<div class="col-lg-4 col-md-6">
-										<div class="post-block-wrapper post-float-half clearfix">
-											<div class="post-content">
-												<a class="post-category" href="#">Otomotif</a>
-												<h2 class="post-title mt-3">
-													<a href="<?php echo BASE_URL; ?>/read.php?title=informasi-lelang-mobil-murah-jakarta-terbaru-2023">Informasi Lelang Mobil Murah Jakarta Terbaru 2023</a>
-												</h2>
-												<div class="post-meta">
-													<span class="posted-time">19 hours</span>
-												</div>
-												<p>
-													Bagi mereka yang tengah mencari mobil dengan anggaran terbatas, Jakarta saat ini menawarkan peluang emas dengan adanya lelang mobil murah. Dalam tahun 2023, Anda dapat menjumpai berbagai pilihan mob
-												</p>
-											</div>
-										</div>
-									</div>
-									<div class="col-lg-4 col-md-6">
-										<div class="post-block-wrapper post-float-half clearfix">
-											<div class="post-content">
-												<a class="post-category" href="#">Nasional</a>
-												<h2 class="post-title mt-3">
-													<a href="<?php echo BASE_URL; ?>/read.php?title=ditjen-pajak-bogor-lelang-mobil-sitaan-innova-dan-avanza-dibanderol-harga-rp-30-jutaan-rp-40-jutaan">Ditjen Pajak Bogor Lelang Mobil Sitaan, Innova dan Avanza Dibanderol Harga Rp 30 Jutaan-Rp 40 Jutaan</a>
-												</h2>
-												<div class="post-meta">
-													<span class="posted-time">19 hours</span>
-												</div>
-												<p>
-													Saat ini Ditjen Pajak Bogor sedang menggelar lelang beberapa unit mobil bekas murah. Tiga di antara merek mobil sitaan yang dilelang yakni Toyota Kijang Innova, Toyota Avanza dan KIA. Menariknya, mobi
-												</p>
-											</div>
-										</div>
-									</div>
-									<div class="col-lg-4 col-md-6">
-										<div class="post-block-wrapper post-float-half clearfix">
-										<div class="post-content">
-										<a class="post-category" href="#">Politik</a>
-										<h2 class="post-title mt-3">
-										<a href="/read/survei-sebaran-elektabilitas-capres-di-jawa-barat">Survei Sebaran Elektabilitas Capres di Jawa Barat</a>
-										</h2>
-										<div class="post-meta">
-										<span class="posted-time">1 day ago</span>
-										</div>
-										<p></p>
-										</div>
-										</div>
-									</div>
-									<div class="col-lg-4 col-md-6">
-										<div class="post-block-wrapper post-float-half clearfix">
-										<div class="post-content">
-										<a class="post-category" href="#">Ekonomi</a>
-										<h2 class="post-title mt-3">
-										<a href="/read/mnc-bank-membuka-peluang-kerja-sama-dengan-koperasi-simpan-pinjam-dalam-penyaluran-kredit-di-tahun-2023">MNC Bank Membuka Peluang Kerja Sama Dengan Koperasi Simpan Pinjam Dalam Penyaluran Kredit di Tahun 2023</a>
-										</h2>
-										<div class="post-meta">
-										<span class="posted-time">1 day ago</span>
-										</div>
-										<p>JAKARTA – PT Bank MNC Internasional Tbk kembali mempererat kerja sama dengan Koperasi Simpan Pinjam Nusantara (Kopnus) dalam penyaluran kredit. Hal ini sebagai upaya perseroan mendorong kinerja peny</p>
-										</div>
-										</div>
-									</div>
-									<div class="col-lg-4 col-md-6">
-										<div class="post-block-wrapper post-float-half clearfix">
-										<div class="post-content">
-										<a class="post-category" href="#">Bisnis</a>
-										<h2 class="post-title mt-3">
-										<a href="/read/sewa-mobil-manual-matic-lepas-kunci-harian-bulanan-bogor-barat-harga-promo">Sewa Mobil Manual Matic Lepas Kunci Harian Bulanan Bogor Barat Harga Promo</a>
-										</h2>
-										<div class="post-meta">
-										<span class="posted-time">1 day ago</span>
-										</div>
-										<p>Kerap dijadikan tujuan berlibur warga ibu kota, Bogor, memang memiliki daya tarik memikat. Gimana nggak, kamu dapat menemukan sejumlah tempat rekreasi dan panorama alam yang menakjubkan di Bogor. Ngga</p>
-										</div>
-										</div>
-									</div>
-									<div class="col-lg-4 col-md-6">
-										<div class="post-block-wrapper post-float-half clearfix">
-										<div class="post-content">
-										<a class="post-category" href="#">Terbaru</a>
-										<h2 class="post-title mt-3">
-										<a href="/read/polisi-gali-keterangan-12-saksi-buru-dpo-pembunuh-wanita-di-bogor">Polisi Gali Keterangan 12 Saksi Buru DPO Pembunuh Wanita di Bogor</a>
-										</h2>
-										<div class="post-meta">
-										<span class="posted-time">2 days</span>
-										</div>
-										<p>Bogor - Polisi memeriksa 12 saksi terkait kasus pembunuhan wanita berinisial R di Tanahsareal, Kota Bogor. Terduga pelaku pembunuhan, Muhamad Rosadi Ramadhan (25), saat ini masih diburu polisi."Untuk </p>
-										</div>
-										</div>
-									</div>
-									<div class="col-lg-4 col-md-6">
-										<div class="post-block-wrapper post-float-half clearfix">
-										<div class="post-content">
-										<a class="post-category" href="#"></a>
-										<h2 class="post-title mt-3">
-										<a href="/read/4-gadis-di-bawah-umur-di-manado-jdi-korban-traffiking-dengan-nama">4 Gadis di bawah Umur di Manado jdi Korban Traffiking, Dengan nama :</a>
-										</h2>
-										<div class="post-meta">
-										<span class="posted-time">1 week ago</span>
-										</div>
-										<p>Nama diatas sudah terindetifikasi alamatnya, pelaku sedang...</p>
-										</div>
-										</div>
-									</div>
-									<div class="col-lg-4 col-md-6">
-										<div class="post-block-wrapper post-float-half clearfix">
-										<div class="post-content">
-										<a class="post-category" href="#"></a>
-										<h2 class="post-title mt-3">
-										<a href="/read/papih-erlov-viral-menjadi-artis-lokal">Papih Erlov viral menjadi Artis lokal</a>
-										</h2>
-										<div class="post-meta">
-										<span class="posted-time">3 weeks</span>
-										</div>
-										<p>Viral artis baru Papih Erlov</p>
-										</div>
-										</div>
-									</div>
-									<div class="col-lg-4 col-md-6">
-										<div class="post-block-wrapper post-float-half clearfix">
-											<div class="post-content">
-												<a class="post-category" href="#"></a>
-												<h2 class="post-title mt-3">
-													<a href="/read/official-trailerhappiness-for-beginners-free-netflix-by-provider">OFFICIAL TRAILER.."Happiness for Beginners" ...free NETFLIX by provider.</a>
-												</h2>
-												<div class="post-meta">
-													<span class="posted-time">3 months</span>
-												</div>
-												<p>A year after her divorce, 32-year-old Helen Carpenter lets her brother persuade her to sign up for a wilderness survival course. Through this experience she discovers that you have to get lost to find</p>
-											</div>
-										</div>
-									</div>
+									<?php
+										echo $news;
+									?>
 								</div>
 							</div>
 						</div>
 						<nav aria-label="pagination-wrapper" class="pagination-wrapper">
 							<ul class="pagination justify-content-center">
-								<li class="page-item active"><a class="page-link" href="?page=1">1</a></li>
-								<li class="page-item "><a class="page-link" href="?page=2">2</a></li>
-								<li class="page-item "><a class="page-link" href="?page=3">3</a></li>
-								<li class="page-item "><a class="page-link" href="?page=4">4</a></li>
-								<li class="page-item">
-									<a class="page-link" href="?page=2" aria-label="Next">
-										<span class="">Next</span>
-										<span aria-hidden="true"><i class="fa fa-angle-double-right ml-2"></i></span>
-									</a>
-								</li>
+							<?php
+								if ($page > 1) {
+									echo '<li class="page-item">
+										<a class="page-link" href="index.php?page=' . ($page - 1) . '" aria-label="Previous">
+											<span class="">Previous</span>
+											<span aria-hidden="true"><i class="fa fa-angle-double-left ml-2"></i></span>
+										</a>
+									</li>';
+								}
+								for ($i = 1; $i <= $npages; $i++) {
+									echo '<li class="page-item "' . ($i == $page ? 'active' : '') . '><a class="page-link" href="index.php?page=' . $i . '">' . $i . '</a></li>';
+								}
+								if ($page < $npages - 1) {
+									echo '<li class="page-item">
+										<a class="page-link" href="index.php?page=' . ($page + 1) . '" aria-label="Next">
+											<span class="">Next</span>
+											<span aria-hidden="true"><i class="fa fa-angle-double-right ml-2"></i></span>
+										</a>
+									</li>';
+								}
+							?>
 							</ul>
 						</nav>
 					</div>
