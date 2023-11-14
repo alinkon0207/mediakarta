de213d288f7a68dce9a34ebca6cb338c03fe341d
ilesoviy <ilisovij861@gmail.com>
2023-11-15 00:25:38 +0900

    Fixed /admin

--- admin/posts/data.php
+++ admin/posts/data.php
@@ -45,7 +45,7 @@
                         '</a>', 
                     "title" => $row['title'] . 
                         '<small>' . 
-                            '<a href="javascript:void(0);" onclick="copyToClipboard(`' . BASE_URL . '/read.php?id=' . $row['id'] . '`)" title="Copy Link">' . 
+                            '<a href="javascript:void(0);" onclick="copyToClipboard(`' . BASE_URL . '/read.php?title=' . $row['permalink'] . '`)" title="Copy Link">' . 
                                 '<svg xmlns="http://www.w3.org/2000/svg" width="17" height="17" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-copy">' . 
                                     '<rect x="9" y="9" width="13" height="13" rx="2" ry="2"></rect>' . 
                                     '<path d="M5 15H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1"></path>' . 
